1e15ff6d4f5df707106d1de6b871e49c4d74c160
Ryan Rauh <rauh.ryan@gmail.com>
2011-07-26 10:18:32 -0500

    tweeked the symlink file

--- symlink.rb
+++ symlink.rb
@@ -1,6 +1,17 @@
 #!/usr/bin/env ruby
+
+HELP = <<END
+
+   ruby symlink [command]
+
+   	install		Will install using your %HOME% environment variable
+
+	{directory} 	if you don't use the install command you need to specify
+			the directory your self. eg; c:\\Users\\MyLogin
+END
+
 def print_help
-   	puts "USAGE: /> ruby symlink.rb {Destination Directory}"
+   	puts HELP
 	exit
 end
 
@@ -10,11 +21,11 @@ if ARGV.first == "/?"
 	print_help
 end
 
-
 Dir.glob("{*,.*}") do |file|
 	excludes = [".","..","symlink.rb",".git"]
-	puts "Symlink #{Dir.pwd.gsub("/","\\")}\\#{file} to #{ARGV.first}" unless excludes.include? file
-	cmd = "mklink #{File.directory?(file) ? "/J" : "/H" } #{ARGV.first}\\#{file} #{Dir.pwd.gsub("/","\\")}\\#{file}"
+	home = ARGV.first == "install" ? ENV["HOME"].gsub("/","\\") : ARGV.first
+	puts "Symlink #{Dir.pwd.gsub("/","\\")}\\#{file} to #{home}" unless excludes.include? file
+	cmd = "mklink #{File.directory?(file) ? "/J" : "/H" } #{home}\\#{file} #{Dir.pwd.gsub("/","\\")}\\#{file}"
 	system("call #{cmd}") unless excludes.include? file
 end
 
